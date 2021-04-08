471c1823e309c2a48a1beea905aee5e1b04e8b27
Lucas Holm <lucas.e.holm@gmail.com>
2021-04-08 12:20:14 +0200

    MTvPrograms fetch and css added

---
+++ src/TvPrograms.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from "react";
+
+const TvPrograms = () => {
+
+    const [programs, setPrograms] = useState([]);
+
+    useEffect(() => {
+        const fetchPrograms = async () => {
+            const programData = await fetch('http://tv-api-p2x2o.ondigitalocean.app/SVT 1.json'
+            );
+
+            const data = await programData.json();
+            setPrograms(programData);
+        };
+        if (!programs.length) {
+            fetchPrograms();
+        }
+    });
+
+    return (
+        <div>
+            <h1 class="channel-title">SVT 1</h1>
+            <ul class="program-list">
+                {programs.map((program) => (
+                    <li class="program-list__item">
+                        {program.start}
+                        <br />
+                        {program.name}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+export default TvPrograms;
